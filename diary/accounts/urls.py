8ad7be5a5c86381c2afc9eae65b3284ca447783b
0504kim <minu0504@g.shingu.ac.kr>
2024-11-26 19:57:39 +0900

    test

--- diary/accounts/urls.py
+++ diary/accounts/urls.py
@@ -5,5 +5,4 @@ from . import views
 urlpatterns = [
     path('login/', views.accounts_login, name='login'),
     path('signup/', views.accounts_signup, name='signup'),
-    # 이렇게 함으로써 순환 참조를 피할 수 있음
 ]
