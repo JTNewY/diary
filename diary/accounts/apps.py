c640d7b08361a7b58f659d715e08e8b50f9019d5
0504kim <minu0504@g.shingu.ac.kr>
2024-12-14 14:28:05 +0900

    oo

--- diary/accounts/apps.py
+++ diary/accounts/apps.py
@@ -1,6 +1,21 @@
 from django.apps import AppConfig
-
+from django.contrib.auth import get_user_model
+from datetime import date
 
 class AccountsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'accounts'
+
+    def ready(self):
+            self.create_dummy_user()
+
+    @staticmethod
+    def create_dummy_user():
+        User = get_user_model()
+        if not User.objects.filter(username='dummyuser').exists():
+            User.objects.create_user(
+                username='dummyuser',
+                email='dummyuser@example.com',
+                password='securepassword123',
+                birth_date=date(1990, 5, 15)
+            )
